e0893eaef13af1beaaa428eb67b2f0e1ed6943b2
Tiryoh <tiryoh@gmail.com>
2019-06-12 14:50:17 +0900

    Update i2c pulse counter test scripts

--- scripts/test_i2ctools.sh
+++ scripts/test_i2ctools.sh
@@ -2,39 +2,23 @@
 
 set -eu
 
-[[ -z $(which i2cget)  ]] && echo "i2ctools are required" && exit 1
+[[ -z $(which i2cget) ]] && echo "i2ctools are required" && exit 1
 
-if [ $# -ne 2  ]; then
+if [ $# -ne 2 ]; then
 	echo "Left Motor status"
 	i2cget -y 1 0x10 0x10 w | sed 's/x\(..\)\(..\)/x\2\1/g'
 	echo "Right Motor status"
 	i2cget -y 1 0x11 0x10 w | sed 's/x\(..\)\(..\)/x\2\1/g'
 else
-	if [ $1 -eq "0"  ]; then
-		echo "Set Left Motor Counter 0"
-		sudo i2cset -y 1 0x10 0x10 0x00 0x00 i
-		sleep 1
+	if [[ $1 =~ ^[0-9]+$ ]]; then
+		echo "Set Left Motor Counter $1"
+		i2cset -y 1 0x10 0x10 $(printf '%04x\n' $1 | sed 's/\(..\)\(..\)/0x\1 0x\2/g') i
 		echo "Left Motor status"
 		i2cget -y 1 0x10 0x10 w | sed 's/x\(..\)\(..\)/x\2\1/g'
 	fi
-	if [ $2 -eq "0"  ]; then
-		echo "Set Right Motor Counter 0"
-		sudo i2cset -y 1 0x11 0x10 0x00 0x00 i
-		sleep 1
-		echo "Right Motor status"
-		i2cget -y 1 0x11 0x10 w | sed 's/x\(..\)\(..\)/x\2\1/g'
-	fi
-	if [ $1 -eq "123"  ]; then
-		echo "Set Left Motor Counter 123"
-		sudo i2cset -y 1 0x10 0x10 0x00 0x7B i
-		sleep 1
-		echo "Left Motor status"
-		i2cget -y 1 0x10 0x10 w | sed 's/x\(..\)\(..\)/x\2\1/g'
-	fi
-	if [ $2 -eq "234"  ]; then
-		echo "Set Right Motor Counter 234"
-		sudo i2cset -y 1 0x11 0x10 0x00 0xEA i
-		sleep 1
+	if [[ $2 =~ ^[0-9]+$ ]]; then
+		echo "Set Right Motor Counter $2"
+		i2cset -y 1 0x11 0x10 $(printf '%04x\n' $2 | sed 's/\(..\)\(..\)/0x\1 0x\2/g') i
 		echo "Right Motor status"
 		i2cget -y 1 0x11 0x10 w | sed 's/x\(..\)\(..\)/x\2\1/g'
 	fi
